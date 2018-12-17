376dca093c41827c441cd102f6178ed328b6e594
YurkaKhymcuk <32388238+YurkaKhymcuk@users.noreply.github.com>
2018-12-18 00:48:04 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,147 @@
+$(document).ready(function(){
+
+$("#unig_fo_crs_customer").owlCarousel({
+     loop: true,
+     items: 5,
+     autoplay: true,
+     nav: true,
+     navText:['<button type="button" class="btn slider-left-btn"> </button>','<button type="button" class="btn slider-right-btn"> </button>'],
+     
+     responsive:{
+     	0: {
+          items: 1
+     	},
+     	640: {
+          items: 2
+     	},
+     	860:{
+     	  items: 3
+     	},
+     	1280: {
+          items: 5
+     	}
+     }
+  });
+
+$("#uniq_fo_landing").owlCarousel({
+     loop: true,
+     items: 3,
+     margin: 30,
+     autoplay: true,
+     nav: true,
+     navText:['<button type="button" class="btn slider-left-btn"> </button>','<button type="button" class="btn slider-right-btn"> </button>'],
+     
+     responsive:{
+     	0: {
+          items: 1
+     	},
+     	640: {
+          items: 2
+     	},
+     	860:{
+     	  items: 3
+     	},
+     	1280: {
+          items: 3
+     	}
+     }
+  });
+
+$("#unig_fo_web_site").owlCarousel({
+     loop: true,
+     items: 3,
+     margin: 30,
+     autoplay: true,
+     nav: true,
+     navText:['<button type="button" class="btn slider-left-btn"> </button>','<button type="button" class="btn slider-right-btn"> </button>'],
+     
+     responsive:{
+     	0: {
+          items: 1
+     	},
+     	640: {
+          items: 2
+     	},
+     	860:{
+     	  items: 3
+     	},
+     	1280: {
+          items: 3
+     	}
+     }
+  });
+
+$("#unig_fo_card_side").owlCarousel({
+     loop: true,
+     items: 3,
+     margin: 30,
+     autoplay: true,
+     nav: true,
+     navText:['<button type="button" class="btn slider-left-btn"> </button>','<button type="button" class="btn slider-right-btn"> </button>'],
+     
+     responsive:{
+     	0: {
+          items: 1
+     	},
+     	640: {
+          items: 2
+     	},
+     	860:{
+     	  items: 3
+     	},
+     	1280: {
+          items: 3
+     	}
+     }
+  });
+
+$("#unig_fo_site_directory").owlCarousel({
+     loop: true,
+     items: 3,
+     margin: 30,
+     autoplay: true,
+     nav: true,
+     navText:['<button type="button" class="btn slider-left-btn"> </button>','<button type="button" class="btn slider-right-btn"> </button>'],
+     
+     responsive:{
+     	0: {
+          items: 1
+     	},
+     	640: {
+          items: 2
+     	},
+     	860:{
+     	  items: 3
+     	},
+     	1280: {
+          items: 3
+     	}
+     }
+  });
+
+$("#unig_fo_other").owlCarousel({
+     loop: true,
+     items: 3,
+     margin: 30,
+     autoplay: true,
+     nav: true,
+     navText:['<button type="button" class="btn slider-left-btn"> </button>','<button type="button" class="btn slider-right-btn"> </button>'],
+     
+     responsive:{
+     	0: {
+          items: 1
+     	},
+     	640: {
+          items: 2
+     	},
+     	860:{
+     	  items: 3
+     	},
+     	1280: {
+          items: 3
+     	}
+     }
+  });
+
+});
+
